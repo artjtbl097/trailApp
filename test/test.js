b8a61f30637cd6e68117a2bb45c2508b02e2f5a9
Anuroop <anuroop.tblr@gmail.com>
2018-03-19 19:33:22 +0530

    nightmare

--- test/test.js
+++ test/test.js
@@ -1,29 +1,29 @@
 var assert = require('assert');
-// const Nightmare = require('nightmare')
-// const nightmare = Nightmare({ show: true })
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true })
 
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
-});
-
-// nightmare
-//   .goto('http://yahoo.com')
-//   .type('form[action*="/search"] [name=p]', 'github nightmare')
-//   .click('form[action*="/search"] [type=submit]')
-//   .wait('#main')
-//   .evaluate(function () {
-//     return document.querySelector('#main .searchCenterMiddle li a').href
-//   })
-//   .end()
-//   .then(function (result) {
-//     console.log(result,"--------------------------")
-//   })
-//   .catch(function (error) {
-//     console.log("Enter")
-//     console.error('Search failed:', error);
+// describe('Array', function() {
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal([1,2,3].indexOf(4), -1);
+//     });
 //   });
+// });
+
+nightmare
+  .goto('http://yahoo.com')
+  .type('form[action*="/search"] [name=p]', 'github nightmare')
+  .click('form[action*="/search"] [type=submit]')
+  .wait('#main')
+  .evaluate(function () {
+    return document.querySelector('#main .searchCenterMiddle li a').href
+  })
+  .end()
+  .then(function (result) {
+    console.log(result,"--------------------------")
+  })
+  .catch(function (error) {
+    console.log("Enter")
+    console.error('Search failed:', error);
+  });
